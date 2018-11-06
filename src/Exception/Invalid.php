ed1260ceab7aebd5b2f788cf8a3b3ab07dba7c75
Alexander Letnikow <reclamme@gmail.com>
2018-11-06 12:31:42 +0200

    Add new Invalid exception for enum type checking

---
+++ src/Exception/Invalid.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Horat1us\Environment\Exception;
+
+use Horat1us\Environment;
+use Throwable;
+
+/**
+ * Class Invalid
+ * @package Horat1us\Environment\Exception
+ */
+class Invalid extends \DomainException implements Environment\Exception
+{
+    /** @var mixed */
+    protected $value;
+
+    public function __construct(?$value, string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        $this->value = $value;
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function __toString(): string
+    {
+        return parent::__toString() . PHP_EOL . "Value: " . var_export($this->value, true);
+    }
+}
